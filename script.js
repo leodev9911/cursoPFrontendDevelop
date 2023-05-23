da5b678ad1c73931bdb8e70a44d7b652ef474d70
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-05-23 18:03:31 -0400

    Hice un toggle para shopping-cart

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-const dropdownClick = document.querySelector('#dropdown-click')
-const dropdownMenu = document.querySelector('#dropdown-menu')
-const mobileMenuClick = document.querySelector('.file-icon__container')
-const mobileMenu = document.querySelector('.mobile-menu__container')
+const dropdownClick = document.querySelector('#dropdown-click');
+const dropdownMenu = document.querySelector('#dropdown-menu');
+const mobileMenuClick = document.querySelector('.file-icon__container');
+const mobileMenu = document.querySelector('.mobile-menu__container');
+const shoppingCartClick = document.querySelector('.shop-car-img__container');
+const shoppingCart = document.querySelector('.shopping-cart__container');
 
-dropdownClick.addEventListener('click', toggleDropdownMenu)
-mobileMenuClick.addEventListener('click', showMobileMenu)
+dropdownClick.addEventListener('click', toggleDropdownMenu);
+mobileMenuClick.addEventListener('click', toggleMobileMenu);
+shoppingCartClick.addEventListener('click', toggleShoppingCart);
 
-function toggleDropdownMenu () {
-    dropdownMenu.classList.toggle('inactive')
+function toggleDropdownMenu () {  
+    const shoppingCartClosed = shoppingCart.classList.contains('inactive');
+    
+    if (!shoppingCartClosed) {
+        shoppingCart.classList.add('inactive');
+    };
+
+    dropdownMenu.classList.toggle('inactive');
 }
 
-function showMobileMenu() {
-    mobileMenu.classList.toggle('inactive')
+function toggleMobileMenu() {  
+    const shoppingCartClosed = shoppingCart.classList.contains('inactive');
+
+    if (!shoppingCartClosed) {
+        shoppingCart.classList.add('inactive');
+    };
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart() {
+    const mobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const dropdownMenuClosed = dropdownMenu.classList.contains('inactive');
+
+    if (!mobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    else if (!dropdownMenuClosed) {
+        dropdownMenu.classList.add('inactive')
+    }
+
+    shoppingCart.classList.toggle('inactive');
 }
