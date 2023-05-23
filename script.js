ef95a34f65fc8043caf34d2f9e69fff5fd386ec7
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-05-23 13:32:23 -0400

    Hice un toggle para dropdown menu y arregle un tema del @media para dropdown menu

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const dropdownClick = document.querySelector('#dropdown-click')
 const dropdownMenu = document.querySelector('#dropdown-menu')
+const mobileMenuClick = document.querySelector('.file-icon__container')
+const mobileMenu = document.querySelector('.mobile-menu__container')
 
 dropdownClick.addEventListener('click', toggleDropdownMenu)
+mobileMenuClick.addEventListener('click', showMobileMenu)
 
 function toggleDropdownMenu () {
     dropdownMenu.classList.toggle('inactive')
+}
+
+function showMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
